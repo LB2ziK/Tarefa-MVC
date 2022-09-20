b9c23d1ebd881dc51bffaa0c9a5f3ecb0cc580d0
LB2ziK <lh.rissilb2@gmail.com>
2022-09-20 11:09:14 -0300

    aplicando algumas alterações

---
+++ App/Controller/Controller.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controller;
+
+abstract class Controller
+{
+    protected static function render($view, $model = null)
+    {
+        $arquivo_view = VIEWS . $view . ".php";
+
+        if(file_exists($arquivo_view))
+            include $arquivo_view;
+        else
+            exit('Arquivo "' . $arquivo_view . '" não encontrado');
+    }
+}
---
+++ App/index.php
@@ -0,0 +1,78 @@
+<?php
+
+$uri_parse = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+
+include 'config.php';
+include 'autoload.php';
+
+use App\Controller\{
+    PessoaController,
+    ProdutoController,
+    CategoriaController,
+};
+
+switch($uri_parse)
+{
+    /*
+    Estrutura:
+    Caso a rota seja "/pessoa"
+    O objeto PessoaControler vai executar a função "index()"
+    */
+
+    //Todos os outros comentários estão em todos os arquivos relacionados a Categoria
+
+    //Rotas de Pessoa
+    case '/pessoa':
+        PessoaController::index();
+    break;
+
+    case '/pessoa/form':
+        PessoaController::form();
+    break;
+
+    case '/pessoa/save':
+        PessoaController::save();
+    break;
+
+    case '/pessoa/excluir':
+        PessoaController::delete();
+    break;
+
+    //Rotas de Produto
+    case '/produto':
+        ProdutoController::index();
+    break;
+
+    case '/produto/form':
+        ProdutoController::form();
+    break;
+
+    case '/produto/save':
+        ProdutoController::save();
+    break;
+
+    case '/produto/excluir':
+        ProdutoController::delete();
+    break;
+
+    //Rotas de Categoria
+    case '/categoria':
+        CategoriaController::index();
+    break;
+
+    case '/categoria/form':
+        CategoriaController::form();
+    break;
+
+    case '/categoria/save':
+        CategoriaController::save();
+    break;
+
+    case '/categoria/excluir':
+        CategoriaController::delete();
+    break;
+
+    default:
+        echo "Erro 404";
+    break;
+}
